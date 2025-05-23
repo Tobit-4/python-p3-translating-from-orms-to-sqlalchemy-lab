680a31fa7b976cde4f18be0404faf021b8f0745a
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-05-23 09:49:50 +0300

    made changes

--- lib/models.py
+++ lib/models.py
@@ -11,3 +11,6 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+def __repr__(self):
+        return f"Dog {self.id}: {self.name}, Breed: {self.breed}"
